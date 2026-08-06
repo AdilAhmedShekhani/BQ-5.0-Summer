717d70a378fcfb8e5e1669b6344770bb878aaebd
AdilAhmedShekhani <adilaziz2009@live.com>
2026-08-06 19:16:53 +0500

    Data Types and Expressions

---
+++ Javascript/Class 01/app.js
@@ -0,0 +1,34 @@
+// alert("Hello Students")
+
+// Data Types
+// String
+
+// alert("Kuch Bhi")
+// alert(123)
+// alert(false)
+
+// Variable
+
+// var num1 = "10";
+// var num2 = 10;
+
+// alert(num1 * num2)
+
+// var firstName = "Adil"
+// var last_name = "Ahmed"
+
+// alert(firstName + "Affan " + last_name)
+
+var num1 = prompt();
+var num2 = prompt();
+var result = (num1 * num2)
+
+// console.log(num1 + num2)
+// console.log(num1 * num2)
+// console.log(num1 / num2)
+// console.log(num1 - num2)
+// console.log(num1 % num2)
+
+// var name = prompt("Please Enter Your Name ?")
+
+console.log(result)
