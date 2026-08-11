0465c3b5d82c06ccb6cbe29b7821e0230382e9ef
AdilAhmedShekhani <adilaziz2009@live.com>
2026-08-11 18:05:16 +0500

    Class Created

--- Javascript/Class 01/app.js
+++ Javascript/Class 01/app.js
@@ -19,9 +19,9 @@
 
 // alert(firstName + "Affan " + last_name)
 
-var num1 = prompt();
-var num2 = prompt();
-var result = (num1 * num2)
+// var num1 = prompt();
+// var num2 = prompt();
+// var result = (num1 * num2)
 
 // console.log(num1 + num2)
 // console.log(num1 * num2)
@@ -31,4 +31,21 @@ var result = (num1 * num2)
 
 // var name = prompt("Please Enter Your Name ?")
 
+// console.log(result)
+
+var num = 4;
+
+var result = --num + num++ - ++num + num + num--
+//            3    + 3     -  5   +  5  +  5 
+
+//  7 ,2,11 ,12 ,10
+
 console.log(result)
+
+// Question no1
+// Write a script to greet your website visitor using JS alert
+// box.
+
+var greet = "Welcome to my website"
+
+alert(greet)
