147b31495f98a1762cdcd172a90773b98ea510c4
AdilAhmedShekhani <adilaziz2009@live.com>
2026-08-18 18:05:21 +0500

    Class Updated

---
+++ Javascript/Class 03/app.js
@@ -0,0 +1,37 @@
+// Muhammad Umer => 0
+// Haris => 0
+// Muhammad Safwan => 0
+// Abbas => 10
+
+
+// var num1 = 10
+// var num2 = 10
+
+// var result = num1 + num2
+// alert(result)
+// console.log(num1 + num2)
+
+// Direct Function call
+// console.log(add(10, 54))
+
+// Variable Call
+// console.log(result)
+
+// Variable Create
+// var result = add(5, 6)
+
+// Funtion Add
+// function add(numa, numbb) {
+//     return numa + numbb
+// }
+
+
+
+// console.log(add(5, 6))
+
+// var result = add(5, 6)
+
+// Arrow Funtion Add
+add = (numa, numbb) => numa + numbb
+
+console.log(add(+prompt("Please Enter Num1"), +prompt("Please Enter Num2")))
