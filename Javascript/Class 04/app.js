0d4ce502bc0c2e76758e919425aac6b931eb6b64
AdilAhmedShekhani <adilaziz2009@live.com>
2026-08-20 18:36:25 +0500

    Arrays Methods

--- Javascript/Class 04/app.js
+++ Javascript/Class 04/app.js
@@ -3,4 +3,27 @@ var student2 = "Ahmed"
 
 var students = ["Hibban", "Affan", "Farhan"]
 
-console.log(students[1])
+// console.log(students[1])
+
+// students[3] = "Haris"
+// students[4] = ""
+
+// console.log(students)
+
+// window.alert("Hello")
+
+students.pop()
+students.push("Omar", "Ash")
+
+students.shift()
+students.unshift("Arsalan")
+
+console.log(students)
+
+students.splice(1, 2, "Muzammil", "Alyan")
+
+console.log(students)
+
+var copyarr = students.slice(0, 1)
+
+console.log(copyarr)
